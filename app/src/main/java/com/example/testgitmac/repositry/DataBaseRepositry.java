5583bd3935b5ad5c90cffabfe4dc7eaae6e4d29d
vishalkamboj <vishalkamboj88@gmail.com>
2019-05-18 10:01:35 +0530

    This is done by Vishal added database repo

---
+++ app/src/main/java/com/example/testgitmac/repositry/DataBaseRepositry.java
@@ -0,0 +1,13 @@
+package com.example.testgitmac.repositry;
+
+import android.util.Log;
+
+public class DataBaseRepositry {
+
+
+    void getData()
+    {
+        Log.d("repositry","This is database repositry");
+    }
+
+}
